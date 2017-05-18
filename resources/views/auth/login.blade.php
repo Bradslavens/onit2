813aa5c99bfe3a34b499124b718109517f813de7
vagrant <vagrant@homestead>
2017-05-18 16:16:15 +0000

    jquery add signer to transaction fields

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
 
-                @include('partials.message');
+                @include('partials.message')
                 
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
--- resources/views/create/transactionFormFields.blade.php
+++ resources/views/create/transactionFormFields.blade.php
@@ -18,9 +18,34 @@
                   </div>
                 @endforeach
 
+                <fieldset>
+                    <legend id="signerLegend")>Form Signers</legend>
+                    
+                    <a href="#" id="newSignerLink">Add a Signer</a>
+                </fieldset>
+
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+
+<script>
+$(document).ready(function(){
+
+    var signerfield = "<div class='form-group'><label for='signerName'>Signers Name</label><input type='text' name='signerName' class='form-control' placeholder='Signer Name'></div><div class='form-group'><label for='signerRole'>Signers Role</label><select name='signerRole' class='form-control'><option value='Buyer'>Buyer</option><option value='Seller'>Seller</option><option value='ListngAgent'>Listing Agent</option><option value='SellingAgent'>Selling Agent</option><option value='Escrow'>Escrow</option></select></div><div class='checkbox'><label><input type='checkbox' name='signed'> Signed</label></div>";
+    
+    $("#newSignerLink").click(function(){
+        
+        event.preventDefault();
+
+        $( "#signerLegend" ).append( signerfield );
+    });
+});
+</script>
+
 @endsection
+
+
