b3bf9287f7f7f56a1289cf6f5caf97da338c07e9
bradslavens <bradslavens@gmail.com>
2017-06-28 22:37:57 +0000

    css home

--- resources/views/index/home.blade.php
+++ resources/views/index/home.blade.php
@@ -10,11 +10,29 @@
             <br>
             <ul class="list-group">
             @foreach($transactions as $transaction)
-              <li class="list-group-item">
-
+              <li class="list-group-item">              
                 <h5 class= "list-group-item-heading"><strong>{{$transaction->address1}}</strong></h5> 
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-items" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="menu-items">
+                    <ul class="nav nav-pills">
+                        <li role="presentation"><a href="{{route('transaction.form.select', ['id' => $transaction->id])}}">
+                                <span class="label label-primary">Resume</span>
+                            </a></li>
+                        <li role="presentation"><a href="{{route('transaction.dashboard', ['id' => $transaction->id])}}">
+                                <span class="label label-info">Dashboard</span>
+                            </a></li>
+                        <li role="presentation"><a href="{{route('checklistItems.show', ['id' => $transaction->id])}}">
+                                <span class="label label-primary">Checklist</span>
+                            </a></li>
+                    </ul>
+                </div>
 
-                <p class="list-group-item-text">
+                {{-- <p class="list-group-item-text">
                         @if($transaction->status == 1)
                        <span class="label label-success">Active</span>
                         @endif
@@ -27,7 +45,7 @@
                         <a href="{{route('checklistItems.show', ['id' => $transaction->id])}}">
                             <span class="label label-primary">Checklist</span>
                         </a>
-                </p>
+                </p> --}}
 
               </li>
             @endforeach
